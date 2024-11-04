8ac1faaf5e28fae0a80857534d2c40ded1ffac49
Dimiura <di.miura@hotmail.com>
2024-11-04 12:37:32 -0300

    changing-style

--- index.js
+++ index.js
@@ -132,3 +132,70 @@ document.addEventListener("DOMContentLoaded", updateActiveLink);
         }
     });
 
+// Particles:
+
+
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 50,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#00ff00" // Verde hacker
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        }
+      },
+      "opacity": {
+        "value": 0.6,
+        "random": false,
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#00ff00", // Verde hacker
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2, // Movimento mais suave
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+      },
+      "modes": {
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        }
+      }
+    },
+    "retina_detect": true
+  });
+
+
+
+  
